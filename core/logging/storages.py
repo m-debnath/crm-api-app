2d7f7c2a4256b6d489fb5b670e41a524a34e2c9b
Mukul Debnath <debnath.mukul@gmail.com>
2022-07-18 00:07:30 +0530

    Kafka API audit logging

--- core/logging/storages.py
+++ core/logging/storages.py
@@ -17,8 +17,12 @@ class KafkaStorage(LoggingStorage):
         kafka_entry["host_ip"] = socket.gethostbyname(socket.gethostname())
 
         kafka_entry["id"] = entry.request.request_id
-        kafka_entry["event_type"] = "api_log"
         kafka_entry["request_path"] = entry.request.full_path
+
+        if kafka_entry["request_path"].startswith("/api"):
+            kafka_entry["event_type"] = "api"
+        else:
+            kafka_entry["event_type"] = "admin_site"
         kafka_entry["@timestamp"] = datetime.datetime.utcnow().isoformat()
         kafka_entry["msecs"] = round(entry.execution_time.total_seconds() * 1000)
         try:
@@ -27,15 +31,18 @@ class KafkaStorage(LoggingStorage):
             kafka_entry["username"] = "None"
 
         kafka_entry["http_method"] = entry.request.method
-        kafka_entry["request_headers"] = json.dumps(entry.request.request_headers)
 
         bp_header_name = settings.BUSINESS_PROCESS_HEADER
         kafka_entry["bp_id"] = entry.response.response.headers.get(bp_header_name)
 
+        kafka_entry["request_headers"] = json.dumps(entry.request.request_headers)
         kafka_entry["request_query_params"] = json.dumps(entry.request.query_params)
         kafka_entry["request_data"] = json.dumps(entry.request.data)
+
         kafka_entry["response_code"] = entry.response.status_code
+        kafka_entry["response_headers"] = str(entry.response.response.headers)
         kafka_entry["response_data"] = json.dumps(entry.response.data)
+
         kafka_entry["env_code"] = settings.DJANGO_ENV
 
         logger.info(json.dumps(kafka_entry))
