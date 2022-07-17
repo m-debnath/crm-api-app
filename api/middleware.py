c07a2c0b4f3917e96ae9a1342812dadb1016fa24
Mukul Debnath <debnath.mukul@gmail.com>
2022-07-17 23:41:21 +0530

    Custom BP Id Header

---
+++ api/middleware.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+import uuid
+
+
+class CustomHeaderMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+
+        bp_header_name = settings.BUSINESS_PROCESS_HEADER
+        request_meta_header = "HTTP_" + bp_header_name.upper().replace("-", "_")
+        if request_meta_header in request.META.keys():
+            response[bp_header_name] = request.META.get(request_meta_header)
+        else:
+            response[bp_header_name] = uuid.uuid4().hex
+        return response
