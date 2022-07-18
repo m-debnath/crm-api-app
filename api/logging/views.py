96a0bccd82252c5f905a8509e0904501b40a7df3
Mukul Debnath <debnath.mukul@gmail.com>
2022-07-18 22:54:20 +0530

    Create logging API

--- api/logging/views.py
+++ api/logging/views.py
@@ -12,12 +12,30 @@ from core.logging.utils import log_performance_to_kafka
 logger = logging.getLogger("requestlogs")
 
 
-@api_view(["POST"])
+@api_view(["GET", "POST"])
 @permission_classes([IsAuthenticated])
 @log_performance_to_kafka
 def log_to_kafka(request):
-    kafka_entry = threading.local()
-    kafka_entry.val = request.data
-    t = threading.Thread(target=logger.info, args=(json.dumps(kafka_entry.val),))
-    t.start()
-    return Response(status=status.HTTP_200_OK)
+    if request.method == "POST":
+        kafka_entry = threading.local()
+        kafka_entry.val = request.data
+        t = threading.Thread(target=logger.info, args=(json.dumps(kafka_entry.val),))
+        t.start()
+        return Response(status=status.HTTP_200_OK)
+    else:
+        # Sample message for post
+        return Response(
+            {
+                "host": "user-agent",
+                "host_ip": "user-ip",
+                "id": "fec724450e154cc88587cd55865ec667",
+                "event_type": "ui|ui-error",
+                "@timestamp": "2022-07-18T17:04:31.038Z",
+                "env_code": "development",
+                "func_name": "react.component.method.name",
+                "request_path": "/your/app/name",
+                "username": "username",
+                "details": "this is a sample logging message for post request",
+            },
+            status=status.HTTP_200_OK,
+        )
