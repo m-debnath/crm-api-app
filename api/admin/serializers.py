f106fd3afc6c8a175c73540947f37901621cecc9
Mukul Debnath <debnath.mukul@gmail.com>
2022-07-18 21:40:24 +0530

    Extend user serializer

--- api/admin/serializers.py
+++ api/admin/serializers.py
@@ -5,4 +5,14 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "first_name", "last_name", "email", "is_staff"]
+        fields = [
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "is_staff",
+            "is_active",
+            "last_login",
+            "date_joined",
+        ]
