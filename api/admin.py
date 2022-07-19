f48d77154377fb935aa54d85420a4c6bd3ee1672
Mukul Debnath <debnath.mukul@gmail.com>
2022-07-19 20:46:01 +0530

    Custom Admin Site Header and Title

--- api/admin.py
+++
@@ -1,5 +0,0 @@
-from django.conf import settings
-from django.contrib import admin
-
-admin.site.site_header = f"{settings.APP_NAME} Administration"
-admin.site.site_title = f"{settings.APP_NAME} Admin"
--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,9 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+
+admin.site.site_header = f"{settings.APP_NAME} Administration"
+admin.site.site_title = f"{settings.APP_NAME} Admin"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
